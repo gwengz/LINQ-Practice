89fb5b01557dd55ba036e5c5c89e82dc7a4fe99c
gwengz <gwengozesuperb@gmail.com>
2025-10-21 17:51:10 +0800

    Add project files.

---
+++ LINQ-Practice/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnClick = New System.Windows.Forms.Button()
+        Me.labelOutput = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnClick
+        '
+        Me.btnClick.Location = New System.Drawing.Point(50, 34)
+        Me.btnClick.Name = "btnClick"
+        Me.btnClick.Size = New System.Drawing.Size(75, 23)
+        Me.btnClick.TabIndex = 0
+        Me.btnClick.Text = "CLICK"
+        Me.btnClick.UseVisualStyleBackColor = True
+        '
+        'labelOutput
+        '
+        Me.labelOutput.AutoSize = True
+        Me.labelOutput.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelOutput.Location = New System.Drawing.Point(161, 43)
+        Me.labelOutput.Name = "labelOutput"
+        Me.labelOutput.Size = New System.Drawing.Size(63, 20)
+        Me.labelOutput.TabIndex = 1
+        Me.labelOutput.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.labelOutput)
+        Me.Controls.Add(Me.btnClick)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnClick As Button
+    Friend WithEvents labelOutput As Label
+End Class
---
+++ LINQ-Practice/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
+        Dim numbers As List(Of Integer) = New List(Of Integer) From {1, 2, 3, 4, 5,
+6}
+        Dim evenNumbers = numbers.Where(Function(n) n Mod 2 = 0)
+        For Each num In evenNumbers
+            Console.WriteLine(num) ' Output: 2, 4, 6
+        Next
+    End Sub
+End Class
