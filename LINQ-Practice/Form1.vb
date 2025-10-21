18c24721e778d8f92ab0ab1d48f9cdc9af049151
gwengz <gwengozesuperb@gmail.com>
2025-10-21 18:25:25 +0800

    OrderBy (Ascending 1-5) OrderByDescending (Descending 5-1)

--- LINQ-Practice/Form1.vb
+++ LINQ-Practice/Form1.vb
@@ -2,7 +2,7 @@
     Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
         Dim names As List(Of String) = New List(Of String) From {"Crystal", "Jamaica", "Carmelo", "Angelo", "Tin", "Jo"}
 
-        Dim upperNames = names.Select(Function(n) n.ToLower())
+        Dim upperNames = names.OrderBy(Function(n) n.ToLower())
 
         Dim outputMessage As String = ""
 
