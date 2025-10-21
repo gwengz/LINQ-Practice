da6ef98b460044b8f19dcc0503778135675f4a63
gwengz <gwengozesuperb@gmail.com>
2025-10-21 18:01:08 +0800

    ForEach method was added

--- LINQ-Practice/Form1.vb
+++ LINQ-Practice/Form1.vb
@@ -1,10 +1,15 @@
 ﻿Public Class Form1
     Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
-        Dim numbers As List(Of Integer) = New List(Of Integer) From {1, 2, 3, 4, 5,
-6}
+        Dim numbers As List(Of Integer) = New List(Of Integer) From {1, 2, 3, 4, 5, 6}
+
         Dim evenNumbers = numbers.Where(Function(n) n Mod 2 = 0)
+
+        Dim outputMessage As String = ""
+
         For Each num In evenNumbers
-            Console.WriteLine(num) ' Output: 2, 4, 6
+            outputMessage += num & " "
         Next
+
+        labelOutput.Text = outputMessage
     End Sub
 End Class
