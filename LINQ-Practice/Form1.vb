d05e0adf3c2209c6bde99e3d22077f8852d101d9
gwengz <gwengozesuperb@gmail.com>
2025-10-21 18:16:42 +0800

    Practicing Lowering the letters

--- LINQ-Practice/Form1.vb
+++ LINQ-Practice/Form1.vb
@@ -1,13 +1,12 @@
 ﻿Public Class Form1
     Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
-        Dim names As List(Of String) = New List(Of String) From {"Crystal", "Jamaica",
-                                                         "Carmelo", "Angelo", "Tin", "Jo"}
+        Dim names As List(Of String) = New List(Of String) From {"Crystal", "Jamaica", "Carmelo", "Angelo", "Tin", "Jo"}
 
-        Dim lessThanFive = names.Where(Function(n) n.Length <= 5)
+        Dim upperNames = names.Select(Function(n) n.ToLower())
 
         Dim outputMessage As String = ""
 
-        For Each namee In lessThanFive
+        For Each namee In upperNames
             outputMessage += namee & " "
         Next
 
